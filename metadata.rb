3d779ff4f5e27ee84c03eef458fb69a70faaeb43
Wes Morgan <cap10morgan@gmail.com>
2012-06-22 11:16:32 -0400

    remove apt-get upgrade stuff for now (on branch v1.1); bump version to 1.0.6

--- metadata.rb
+++ metadata.rb
@@ -3,15 +3,12 @@ maintainer_email "cap10morgan@gmail.com"
 license          "Apache 2.0"
 description      "Collection of useful recipes for setting up Linodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.0.6"
 
 recipe           "default", "Sets up static networking & DNS"
-recipe           "apt_get_upgrade", "Upgrades packages on first boot"
 recipe           "api_gem", "Installs the Linode API client for the other recipes to work"
 recipe           "static_net", "Sets up static networking"
 recipe           "dns", "Creates Linode DNS entries for the node"
-
-depends "apt"
 
 %w{ubuntu debian}.each do |os|
   supports os
